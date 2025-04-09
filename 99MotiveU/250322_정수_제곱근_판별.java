b2e3737858369b9111d80c4b83313764ad2cc5a4
Dong ki <119654357+99MotiveU@users.noreply.github.com>
2025-04-09 20:50:12 +0900

    Delete 99MotiveU directory

--- 99MotiveU/250322_정수_제곱근_판별.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public long solution(long n) {
-        long answer = -1;
-        long d = (long)Math.sqrt(n);
-        if(d*d==n){answer = (d+1)*(d+1);}
-        return answer;
-        
-    }
-}
